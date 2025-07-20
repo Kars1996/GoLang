b65758a98117012d7a94abff377bf1568c66f94b
Kars <k@kars.bio>
2025-07-20 16:44:36 +0100

    arrays are a bit weird but we go

---
+++ array-shit.go
@@ -0,0 +1,12 @@
+package main;
+
+import "fmt";
+
+func main () {
+	var favNums [3]int;
+
+	favNums[0] = 1;
+	favNums[1] = 2;
+	favNums[2] = 3; // pirate software ahh code
+	fmt.Println(favNums[0])
+}
