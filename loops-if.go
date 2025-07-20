96da24ae9c1849fba12eeb2821fe33ec40acde7d
Kars <k@kars.bio>
2025-07-20 18:33:08 +0100

    variable stuff

--- loops-if.go
+++ loops-if.go
@@ -12,4 +12,10 @@ func main() {
 	} else {
 		fmt.Println("Fuck off twat");
 	}
+
+	coolBeans := [5]string {"Me", "Myself", "I", "MeSelf", "ISelf"}
+
+	for index, value := range coolBeans {
+		fmt.Printf("%s is the %dst coolest bean\n", value, index)
+	}
 }
